7bccfe5261824c8675dd1a6e3e707a6a21ffc241
Jon Calhoun <joncalhoun@gmail.com>
2015-02-12 19:22:03 -0500

    impl messaging all sockets at once and removing connection if it disconnects

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"code.google.com/p/go.net/websocket"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"time"
@@ -12,6 +13,8 @@ func main() {
 	server := NewServer()
 	http.Handle("/ws", server.GetHandler())
 	http.Handle("/", http.FileServer(http.Dir("public")))
+
+	go PingSockets()
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
 
@@ -23,19 +26,52 @@ func NewServer() *Server {
 
 var Connections []*websocket.Conn
 
+func PingSockets() {
+	for {
+		time.Sleep(5 * time.Second)
+
+		msg := map[string]string{
+			"Message": fmt.Sprintf("%d connections!", len(Connections)),
+		}
+		for _, c := range Connections {
+			err := websocket.JSON.Send(c, msg)
+			if err != nil {
+				log.Println(err)
+			}
+		}
+	}
+}
+
+func removeConnection(ws *websocket.Conn) {
+	newConn := []*websocket.Conn{}
+	for _, c := range Connections {
+		if c != ws {
+			newConn = append(newConn, c)
+		}
+	}
+	Connections = newConn
+}
+
 func (s *Server) GetHandler() websocket.Handler {
 	onConnect := func(ws *websocket.Conn) {
 		Connections = append(Connections, ws)
 
 		for {
-			msg := map[string]string{
-				"Message": fmt.Sprintf("%d connections!", len(Connections)),
+			select {
+			default:
+				var msg struct {
+					Message string `json:"message"`
+				}
+				err := websocket.JSON.Receive(ws, &msg)
+				if err == io.EOF {
+					removeConnection(ws)
+					return
+				} else if err != nil {
+					log.Println(err)
+				} else {
+					log.Printf("Received: %+v\n", msg)
+				}
 			}
-			err := websocket.JSON.Send(ws, msg)
-			if err != nil {
-				log.Println(err)
-			}
-			time.Sleep(2 * time.Second)
 		}
 	}
 
